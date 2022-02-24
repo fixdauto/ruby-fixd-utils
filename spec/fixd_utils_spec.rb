fbb7cf0828dd8659a87e84d9e66227e3b3b46098
Charles Julian Knight <charles@rabidaudio.com>
2022-02-24 15:33:31 -0500

    initial commit

---
+++ spec/fixd_utils_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FixdUtils do
+  it "has a version number" do
+    expect(FixdUtils::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
