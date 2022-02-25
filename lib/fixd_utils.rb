f962217d686dda64fce0fc128ab774d788e4e53c
Charles Julian Knight <charles@rabidaudio.com>
2022-02-25 11:07:54 -0500

    oops

--- lib/fixd_utils.rb
+++ lib/fixd_utils.rb
@@ -2,7 +2,6 @@
 
 require_relative "active_record_extensions"
 require_relative "base_32"
-require_relative "data_struct"
 require_relative "global_lock"
 require_relative "network_error"
 require_relative "uri_builder"
